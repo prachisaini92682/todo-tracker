e403a0c7b69c399fce7953cb04d3740f0c8b56de
Prachi Saini <prachisaini92682@gmail.com>
2024-03-01 20:47:20 +0530

    remove temp changes

--- app/page.js
+++ app/page.js
@@ -38,7 +38,7 @@ const page = () => {
             }}
             className="bg-red-400 text-white px-4 py-2 rounded font-bold"
           >
-            Delete todo
+            Delete
           </button>
         </li>
       );
